e2dbd0b23aa34fa330a568eea00ce59fb25ad4a3
Cristian Ciobanu <cristian.ciobanu@amdaris.com>
2019-02-08 11:55:04 +0200

    Test

---
+++ C#/03. OOP Advanced/OOPAdvanced/OOPAdvanced/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace OOPAdvanced
+{
+    interface ILogin
+    {
+        void Login();
+    }
+    interface IRegister
+    {
+        void Register();
+    }
+    interface IModerator
+    {
+        void Advertisment();
+    }
+    interface IMessage
+    {
+        void WriteMessage();
+    }
+    class User:ILogin, IRegister, IMessage
+    {
+        public int id { set; get; }
+        public string name { set; get; }
+        public string password { get; set; }
+        public void Login()
+        {
+            Decrypt();
+            Console.WriteLine("User Login");
+        }
+        public void Register()
+        {
+            Encrypt();
+            Console.WriteLine("User Register");
+        }
+        private void Encrypt() => Console.WriteLine("Encrypt Password");
+        private void Decrypt() => Console.WriteLine("Decrypt password");
+        public virtual void WriteMessage() => Console.WriteLine("Write Message");
+    }
+    class Admin:User
+    {
+        public void SetPrivileges() => Console.WriteLine("Set privileges");
+        public override void WriteMessage()
+        {
+            Console.WriteLine("Message from Admin")
+        }
+    }
+    class Moderator:User, IModerator
+    {
+        public void DeleteMessage() => Console.WriteLine("Delete Message");
+        public void Advertisment() => Console.WriteLine("Advertisment");
+    }
+    class SimplyUser:User
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
